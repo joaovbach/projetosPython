dd14ddaf22a714cdb8217d125eb2487024d2baf7
joaovbach <joaovbach@github.com>
2022-02-02 10:08:17 -0400

    entra na aba de demandas corrigido

--- criademanda.py
+++ criademanda.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from playwright.sync_api import Playwright, sync_playwright
 import time
 
@@ -7,13 +6,4 @@ def run(playwright, page):
     page.click("button:has-text(\"Demandas\")")
     print("aguardando")
     time.sleep(10000)
-=======
-import playwright
 
-class criaDemanda:
-    def __init__(self) -> None:
-        print("criador de demanda instanciado")
-
-    def status(self):
-        print("criando demanda...")
->>>>>>> e61bbceabaf52ed0bd10f0c0bb89f4b9702786a7
--- selecioneAutomacoes.py
+++ selecioneAutomacoes.py
@@ -1,23 +1,14 @@
 import login
-<<<<<<< HEAD
 import criademanda
 import time
-
 from playwright.sync_api import Playwright, sync_playwright
 
 
-
-=======
-from playwright.sync_api import Playwright, sync_playwright
-
-#login.run(sync_playwright())
->>>>>>> e61bbceabaf52ed0bd10f0c0bb89f4b9702786a7
- 
 while 1:
     print("1-fazer login")
     opcao = input("digite a opcao")
     if opcao == '1':
-<<<<<<< HEAD
+
         print("oisdikosajhasdkjhaskojdhaslkjdhaskljdhaskdjhasjkdh")
         with sync_playwright() as playwright:
             browser = playwright.chromium.launch(headless=False)
@@ -25,7 +16,4 @@ while 1:
             page = context.new_page()
             login.run(playwright, page)
 
-=======
-        with sync_playwright() as playwright:
-            login.run(playwright)
->>>>>>> e61bbceabaf52ed0bd10f0c0bb89f4b9702786a7
+
